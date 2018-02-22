ed358caa64fd41c966df1f2572d8089a62bd380c
lanae <rlfoard@gmail.com>
2018-02-22 13:19:59 -0700

    added questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,16 +4,72 @@ $(document).ready(function () {
         [
             // question 1
             {
-                "answer": [1, "dog"],
-                "question": "What is the best type of animal?",
-                "options": ["cat", "dog", "whale", "sloth"],
+                "answer": [1, "776 BC"],
+                "question": "When were the first ancient olympic games held?",
+                "options": ["980 BC", "776 BC", "33 AD", "200 BC"],
                 // "image": "../images/80s.gif"
             },
             // question 2
             {
-                "answer": [2, "spongy"],
-                "question": "What color is the air",
-                "options": ["yellow", "vacant", "spongy", "pink"],
+                "answer": [2, "Zeus"],
+                "question": "The ancient olympig games were a festival celebrating which god?",
+                "options": ["Athena", "Apollo", "Zeus", "Achilles"],
+
+            },
+            // question 3
+            {
+                "answer": [3, "1896"],
+                "question": "When were the first modern-day olympics held?",
+                "options": ["1784", "1688", "1910", "1896"],
+
+            },
+            // question 4
+            {
+                "answer": [1, "1924"],
+                "question": "When were the first winter olympics held?",
+                "options": ["1910", "1924", "1896", "1940"],
+
+            },
+            // question 5
+            {
+                "answer": [2, "Norway"],
+                "question": "Which country has won the most medals at the winter olympics?",
+                "options": ["United States", "Russia", "Norway", "China"],
+
+            },
+            // question 6
+            {
+                "answer": [0, "United States"],
+                "question": "Which country has won the most medals at the summer olympics?",
+                "options": ["United States", "Brazil", "France", "Spain"],
+
+            },
+            // question 7
+            {
+                "answer": [2, "8"],
+                "question": "How many times has the United States hosted the olympics?",
+                "options": ["4", "9", "8", "6"],
+
+            },
+            // question 8
+            {
+                "answer": [1, "1980"],
+                "question": "Which year did the United States boycott the olympics?",
+                "options": ["1964", "1940", "1980", "1930"],
+
+            },
+            // question 9
+            {
+                "answer": [2, "Michael Phelps"],
+                "question": "Who holds the most olympic medals?",
+                "options": ["Shawn White", "Larisa Latynina", "Michael Phelps", "Jenny Thompson"],
+
+            },
+            // question 10
+            {
+                "answer": [1, "Middle East"],
+                "question": "Each of the Olympic rings represents a region of the world. Which of the following is not a region represented by an Olympic ring?",
+                "options": ["Africa", "Middle East", "Oceana", "Europe"],
 
             },
         ]
@@ -33,12 +89,13 @@ $(document).ready(function () {
 
     function initializeGame() {
         $("#start").text("Click here to begin");
+        index = 0;
     }
 
 
 
     function startNewGame() {
-        index = 0;
+        // index = 0;
         renderQuestion();
     }
 
@@ -102,6 +159,20 @@ $(document).ready(function () {
 
     }
 
+    function gameOver() {
+        $("#result").hide();
+        $("#correct").hide();
+        $("#game-over").show();
+        $("#number-correct").show();
+        $("#number-incorrect").show();
+        $("#number-unanswered").show();
+        $("#play-again").show();
+        $("#game-over").append("Game over! See how you did!");
+        $("#number-correct").append("Number of correct answers: " + correct);
+        $("#number-incorrect").append("Number of incorrect answers: " + incorrect);
+        $("number-unanswered").append("Number of unanswered questions: " + unanswered);
+    }
+
 
     function countdown() {
         if (timeLeft === 0) {
@@ -113,13 +184,17 @@ $(document).ready(function () {
         }
     }
 
-    // renderQuestion();
-    initializeGame();
+    renderQuestion();
+    // initializeGame();
     // startNewGame();
 
     $("#start").on("click", function () {
         startNewGame();
     });
+
+    $("#play-again").on("click", function () {
+        startNewGame();
+    })
 
     $(".answer").on("click", function () {
         $("#correct").append("The correct answer is " + questionArray[index].answer[1]);
