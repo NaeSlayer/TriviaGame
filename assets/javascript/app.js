3ab2920e2063d0cb96071c17b95e9a61d74b76b8
lanae <rlfoard@gmail.com>
2018-05-24 09:51:33 -0700

    empty divs on results screen on game restart

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,87 +1,86 @@
 $(document).ready(function () {
 
-    var questionArray =
-        [
-            // question 1
-            {
-                "answer": [1, "776 BC"],
-                "question": "When were the first ancient Olympic Games held?",
-                "options": ["980 BC", "776 BC", "33 AD", "200 BC"],
-                "image": "assets/images/acropolis.jpg",
-            },
-            // question 2
-            {
-                "answer": [2, "Zeus"],
-                "question": "The ancient Olympic Games were a festival celebrating which god?",
-                "options": ["Athena", "Apollo", "Zeus", "Achilles"],
-                "image": "assets/images/zeus.jpg",
+    var questionArray = [
+        // question 1
+        {
+            "answer": [1, "776 BC"],
+            "question": "When were the first ancient Olympic Games held?",
+            "options": ["980 BC", "776 BC", "33 AD", "200 BC"],
+            "image": "assets/images/acropolis.jpg",
+        },
+        // question 2
+        {
+            "answer": [2, "Zeus"],
+            "question": "The ancient Olympic Games were a festival celebrating which god?",
+            "options": ["Athena", "Apollo", "Zeus", "Achilles"],
+            "image": "assets/images/zeus.jpg",
 
-            },
-            // question 3
-            {
-                "answer": [3, "1896"],
-                "question": "When were the first modern-day Olympics held?",
-                "options": ["1784", "1688", "1910", "1896"],
-                "image": "assets/images/acropolis.jpg",
+        },
+        // question 3
+        {
+            "answer": [3, "1896"],
+            "question": "When were the first modern-day Olympics held?",
+            "options": ["1784", "1688", "1910", "1896"],
+            "image": "assets/images/acropolis.jpg",
 
-            },
-            // question 4
-            {
-                "answer": [1, "1924"],
-                "question": "When were the first Winter Olympics held?",
-                "options": ["1910", "1924", "1896", "1940"],
-                "image": "assets/images/winter.jpg",
+        },
+        // question 4
+        {
+            "answer": [1, "1924"],
+            "question": "When were the first Winter Olympics held?",
+            "options": ["1910", "1924", "1896", "1940"],
+            "image": "assets/images/winter.jpg",
 
-            },
-            // question 5
-            {
-                "answer": [2, "Norway"],
-                "question": "Which country has won the most medals at the Winter Olympics?",
-                "options": ["United States", "Russia", "Norway", "China"],
-                "image": "assets/images/bobsled.jpg",
+        },
+        // question 5
+        {
+            "answer": [2, "Norway"],
+            "question": "Which country has won the most medals at the Winter Olympics?",
+            "options": ["United States", "Russia", "Norway", "China"],
+            "image": "assets/images/bobsled.jpg",
 
-            },
-            // question 6
-            {
-                "answer": [0, "United States"],
-                "question": "Which country has won the most medals at the Summer Olympics?",
-                "options": ["United States", "Brazil", "France", "Spain"],
-                "image": "assets/images/medals.jpg",
+        },
+        // question 6
+        {
+            "answer": [0, "United States"],
+            "question": "Which country has won the most medals at the Summer Olympics?",
+            "options": ["United States", "Brazil", "France", "Spain"],
+            "image": "assets/images/medals.jpg",
 
-            },
-            // question 7
-            {
-                "answer": [2, "8"],
-                "question": "How many times has the United States hosted the Olympics?",
-                "options": ["4", "9", "8", "6"],
-                "image": "assets/images/olympic-stairs.jpg",
+        },
+        // question 7
+        {
+            "answer": [2, "8"],
+            "question": "How many times has the United States hosted the Olympics?",
+            "options": ["4", "9", "8", "6"],
+            "image": "assets/images/olympic-stairs.jpg",
 
-            },
-            // question 8
-            {
-                "answer": [2, "1980"],
-                "question": "Which year did the United States boycott the Olympics?",
-                "options": ["1964", "1940", "1980", "1930"],
-                "image": "assets/images/sculpture.jpg",
+        },
+        // question 8
+        {
+            "answer": [2, "1980"],
+            "question": "Which year did the United States boycott the Olympics?",
+            "options": ["1964", "1940", "1980", "1930"],
+            "image": "assets/images/sculpture.jpg",
 
-            },
-            // question 9
-            {
-                "answer": [2, "Michael Phelps"],
-                "question": "Who holds the most Olympic medals?",
-                "options": ["Shawn White", "Larisa Latynina", "Michael Phelps", "Jenny Thompson"],
-                "image": "assets/images/michael-phelps.jpg",
+        },
+        // question 9
+        {
+            "answer": [2, "Michael Phelps"],
+            "question": "Who holds the most Olympic medals?",
+            "options": ["Shawn White", "Larisa Latynina", "Michael Phelps", "Jenny Thompson"],
+            "image": "assets/images/michael-phelps.jpg",
 
-            },
-            // question 10
-            {
-                "answer": [1, "Middle East"],
-                "question": "Each of the Olympic rings represents a region of the world. Which of the following is not a region represented by an Olympic ring?",
-                "options": ["Africa", "Middle East", "Oceana", "Europe"],
-                "image": "assets/images/ground.jpg",
+        },
+        // question 10
+        {
+            "answer": [1, "Middle East"],
+            "question": "Each of the Olympic rings represents a region of the world. Which of the following is not a region represented by an Olympic ring?",
+            "options": ["Africa", "Middle East", "Oceana", "Europe"],
+            "image": "assets/images/ground.jpg",
 
-            },
-        ]
+        },
+    ]
 
     var questions;
     var option;
@@ -157,8 +156,7 @@ $(document).ready(function () {
                 console.log($(this).data('correct'));
                 if ($(this).data("correct") == 1) {
                     loadAnswerPage("correct");
-                }
-                else {
+                } else {
                     loadAnswerPage("incorrect");
                 }
 
@@ -190,13 +188,11 @@ $(document).ready(function () {
             $("#result").text("Correct");
             $("#result").css("color", "green");
             correct++;
-        }
-        else if (answer === "Time's Up") {
+        } else if (answer === "Time's Up") {
             $("#result").text("You are out of time!!!!");
 
             unanswered++
-        }
-        else {
+        } else {
             (answer === "incorrect");
             $("#result").text("Incorrect");
             $("#result").css("color", "red");
@@ -247,6 +243,11 @@ $(document).ready(function () {
 
     $("#play-again").on("click", function () {
         startNewGame();
+        $("#game-over").empty();
+        $("#number-correct").empty();
+        $("#number-incorrect").empty();
+        $("#number-unanswered").empty();
+        $("#play-again").empty();
     })
 
 
